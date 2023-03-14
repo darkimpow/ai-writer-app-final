ee7465c3ef65f9b005052a78e6dd421424319093
darkimpow <darkimpowell@gmail.com>
2023-03-14 14:52:57 -0400

    added footer & navbar with respective buttons needed for ui & created partials folder for the site shell

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Navbar from "./partials/Navbar";
+import Footer from "./partials/Footer";
 
 function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <Navbar/>
+        <Footer/>
     </div>
   );
 }
