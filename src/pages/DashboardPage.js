fd5bd6fb076e6761839d41cd682a7e227975e181
darkimpow <darkimpowell@gmail.com>
2023-03-16 18:07:13 -0400

    added svg files and text content to main popular category section

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -14,9 +14,39 @@ const DashboardPage = () => {
                 <button className="rounded-full bg-slate-200 py-2 px-6 mr-2">Website</button>
             </div>
             <div className="flex justify-center items-center pt-16">
-                <div className="w-64 mr-6 p-4 rounded-md bg-gray-300">Box 1</div>
-                <div className="w-64 mr-6 p-4 rounded-md bg-gray-400">Box 2</div>
-                <div className="w-64 mr-6 p-4 rounded-md bg-gray-500">Box 3</div>
+                <button className="flex items-center w-72 h-20 mr-11 p-4 rounded-md bg-blue-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" className="w-6 h-6 mr-3">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+                    </svg>
+                <div className="flex flex-col">
+                    <p className="font-bold pr-28">Article & Blog</p>
+                    <p className="text-xs pr-1">Write your dream SEO article short time.</p>
+                </div>
+                </button>
+                <button className="flex items-center w-72 h-20 mr-11 p-4 rounded-md bg-green-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" className="w-6 h-6 mr-3">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"/>
+                    </svg>
+                    <div className="flex flex-col">
+                        <p className="font-bold pr-28">Social Media</p>
+                        <p className="text-xs pr-6">Write compelling detailed product</p>
+                    </div>
+                </button>
+                <button className="flex items-center w-72 h-20 mr-6 p-4 rounded-md bg-purple-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" className="w-6 h-6 mr-3">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"/>
+                    </svg>
+                    <div className="flex flex-col">
+                        <p className="font-bold">Keyword Research</p>
+                        <p className="text-xs pr-2">Pick your exact keyword.</p>
+                    </div>
+                </button>
             </div>
         </div>
     );
