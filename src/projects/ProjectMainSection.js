b9c9ae78efbf7febbdeef3c2e0e370f857b76120
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-03-18 21:35:37 -0400

    initial components in projects folder

---
+++ src/projects/ProjectMainSection.js
@@ -0,0 +1,17 @@
+// example:
+
+import React from 'react';
+////
+////
+
+export const ProjectMainSection = () => {
+//// <Header>
+
+    return (
+        <div>
+            <p>Main section</p>
+        </div>
+    )
+
+//// <Footer>
+}
---
+++ src/projects/ProjectSideBar.js
@@ -0,0 +1,16 @@
+// example:
+import React from 'react';
+////
+////
+
+export const ProjectSideBar = () => {
+//// <Header>
+
+    return (
+        <div>
+            <p>Sidebar</p>
+        </div>
+    )
+
+//// <Footer>
+}
