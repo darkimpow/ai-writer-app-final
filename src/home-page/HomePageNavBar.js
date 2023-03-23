e550d7285954f2d03b5c2b6d8374c18a6172d32c
solyDenis <solydenis38@gmail.com>
2023-03-23 14:48:01 -0400

    updated the home page

---
+++ src/home-page/HomePageNavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from "./logo.png"
+const HomePageNavBar = () => {
+    return (
+        <div>
+
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <img src={logo} className="w-28 h-10"/>
+                </div>
+                <div className="navbar-center">
+                    <div>
+                        <a className="btn btn-ghost normal-case font-bold text-indigo-900 text-xl">About Us</a>
+                    </div>
+                    <div>
+                        <a className="btn btn-ghost normal-case font-bold text-indigo-900 text-xl">Contact Us</a>
+                    </div>
+
+                </div>
+                <div className="navbar-end">
+                    <button>
+                        <a className="btn btn-ghost normal-case font-bold text-indigo-300 text-xl">Sign In</a>
+                    </button>
+                    <button className="btn btn-ghost ">
+                        <div>
+                            <a className="btn btn-ghost normal-case font-bold bg-blue-900 text-white text-xl">Sign Up</a>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePageNavBar;
