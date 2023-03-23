9cd61eef20f820b2ee76d55be3a9a0901e4ae396
Dele Omotosho <dele@authoritywit.com>
2023-03-23 17:09:55 -0400

    chore(routes): adds the relevant framework to route
    Note to all: All future routes should be added to the App.js. Sell L12-16 for reference.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import HomePage from "./pages/HomePage";
-import DashboardPage from "./pages/DashboardPage";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage/>
-    },
-    {
-        path: '/dashboard',
-        element: <DashboardPage/>
-    }
-]);
 
 root.render(
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
 
