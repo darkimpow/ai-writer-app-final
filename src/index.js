f14f02eb17e690cec98911c1d31ee584e7f7e3dc
Dele Omotosho <dele@authoritywit.com>
2023-03-23 11:50:17 -0400

    adds routing for home page and dashboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage/>
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardPage/>
+    }
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router}/>
 );
 
