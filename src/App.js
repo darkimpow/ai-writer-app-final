eb8ee3d3827d3d7082abdfec8cefec14d9be0885
solyDenis <solydenis38@gmail.com>
2023-03-24 10:58:11 -0400

    Merge branch 'main' into feature/home-page-nav-bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {Route, Routes} from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import DashboardMainSection from "./pages/DashboardMainSection";
 
 
 
 function App() {
-<
+
     return (
         <Routes>
             <Route path={'/'} element={<HomePage/>}/>
