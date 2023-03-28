dd08a152ec98b9964da29c9203e91c3046f54d0e
solyDenis <solydenis38@gmail.com>
2023-03-28 11:09:08 -0400

    made the Google button its own component

---
+++ src/component/GoogleButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GoogleButton = () => {
+    return (
+        <div>
+            <button className=" flex justify-center text-black font-bold border-2 rounded-lg py-2 border-b-gray-300 gap-2" ref={gmailMeRef}>
+                <img className="w-6 h-6" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"/>
+                Sign in with Google
+            </button>
+        </div>
+    );
+};
+
+export default GoogleButton;
