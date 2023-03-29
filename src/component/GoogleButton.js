563da07ca0ed965c8a625f5561b38f3d8ddb7b22
solyDenis <solydenis38@gmail.com>
2023-03-29 13:47:42 -0400

    Merge branch 'main' into feature/login
    # Conflicts:
    #	src/App.js
    #	src/component/GoogleButton.js

--- src/component/GoogleButton.js
+++ src/component/GoogleButton.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-<<<<<<< HEAD
-const GoogleButton = () => {
-    return (
-        <div>
-            <button className=" flex justify-center text-black font-bold border-2 rounded-lg py-2 border-b-gray-300 gap-2" ref={gmailMeRef}>
-=======
+
 
 const GoogleButton = () => {
     return (
         <div>
             <button className="w-full flex justify-center text-black font-bold border-2 rounded-lg py-2 border-b-gray-300 gap-2" >
->>>>>>> main
                 <img className="w-6 h-6" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"/>
                 Sign in with Google
             </button>
