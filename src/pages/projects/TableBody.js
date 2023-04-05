ac375090ea7664f316e52d8bcb5432d1c9c5d5f1
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-04-05 00:56:13 -0400

    table paddings

--- src/pages/projects/TableBody.js
+++ src/pages/projects/TableBody.js
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import TableHeader from "./TableHeader";
-import supabase from "../../config/supabase";
 
 export const TableBody = () => {
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        getArticles();
-    }, []);
-
-    const getArticles = async () => {
-        const { data: articles, error } = await supabase
-            .from('articles')
-            .select('*')
-            .filter('profile_id', 'eq', 10)
-            .range(0, 19);
-
-        if (error) {
-            console.error(error);
-            return;
-        }
-
-        setArticles(articles);
-    };
-
-
     return (
-        <div className={'bg-[#faf9f9] w-full'}>
-<div className={'invisible'}>0</div>
+        <div className={'bg-[#faf9f9] w-full pt-3'}>
+
             {/*title - search - filter*/}
             <TableHeader />
 
@@ -54,8 +31,7 @@ export const TableBody = () => {
                     </tr>
                     </thead>
                     <tbody>
-
-                    {/*row 1*/}
+                    {/* row 1 */}
                     <tr>
                         <th>
                             <label>
@@ -334,43 +310,6 @@ export const TableBody = () => {
                             </div>
                         </td>
                     </tr>
-
-
-                    {/* row 0 test */}
-                    {articles.map((article) => (
-                        <tr key={article.id}>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox checkbox-sm rounded" />
-                                </label>
-                            </th>
-                            <td>{article.title}</td>
-                            <td className={'text-gray-400'}>{article.content}</td>
-                            <td>10</td>
-                            <td className={'text-gray-400'}>
-                                <div>
-                                    February 28, 2022
-                                </div>
-                                <div>
-                                    10:25 AM
-                                </div>
-                            </td>
-                            <td className={'flex flex-row items-center justify-center'}>
-                                <div className={'w-8 h-7 mt-3 mb-2 bg-blue-100 flex items-center justify-center rounded-sm mx-1'}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#216bfe" className="w-4 h-4">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                    </svg>
-                                </div>
-                                <div className={'w-8 h-7 mt-3 mb-2 bg-red-100 flex items-center justify-center rounded-sm'}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fe5771" className="w-4 h-4">
-                                        <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
-                                    </svg>
-                                </div>
-                            </td>
-                        </tr>))}
-
-
-
 
                     </tbody>
                 </table>
