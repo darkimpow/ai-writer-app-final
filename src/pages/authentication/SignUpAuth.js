ad0b2657e1dd00c18643e5c5ec0823b71b52e7ac
dladyj <dladyj@gmail.com>
2023-04-05 14:44:58 -0400

    fixed signUpEmailPassword in SignUpAuth.js

--- src/pages/authentication/SignUpAuth.js
+++ src/pages/authentication/SignUpAuth.js
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
-import {supabase} from './supabaseClient';
+import {supabase} from '/supabaseClient';
 import {useNavigate} from "react-router-dom";
+// import data from "bootstrap/js/src/dom/data";
 
 
 const SignUpAuth = () => {
@@ -19,6 +20,12 @@ const SignUpAuth = () => {
             navigate('dashboard/signup');
             console.log('am i logged in or out?');
         });
+        signUpEmailPassword().then((data) => {
+            if (data.error) navigate('/dashboard/signup');
+            navigate('/dashboard/signup');
+            console.log('password');
+        })
+
     };
     const signUpUser = async () => {
         // console.log('all good')
@@ -40,16 +47,15 @@ const SignUpAuth = () => {
         //
         //     };
 
-    const signUpEmailPassword = async () => {
-        let { data, error } = await supabase.auth.signInWithPassword({
+     const signUpEmailPassword = async () => {
+         console.log('almost done');
+        const { data, error } = await supabase.auth.signInWithPassword({
             email: 'example@email.com',
             password: 'example-password',
         })
-        return {data, error}
-        console.log('almost done');
+         return {data, error}
 
     };
-
 
             return (
                 <div>
@@ -60,8 +66,6 @@ const SignUpAuth = () => {
                     </form>
                 </div>
             )
-        }
-
-
+}
 
 export default SignUpAuth;
