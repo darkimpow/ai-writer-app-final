6065fb0a9c46e46bfafa03d18040de5f49165eeb
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-04-07 11:35:14 -0400

    links to home page and all projects

--- src/partials/Navbar.js
+++ src/partials/Navbar.js
@@ -1,5 +1,5 @@
 import {supabase} from "../config/supabase";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="flex-1 pl-3">
-                <a className="btn btn-ghost normal-case text-2xl text-purple-800 font-bold">TexoAi</a>
+                <Link to={'/dashboard/home'} className="btn btn-ghost normal-case text-2xl text-purple-800 font-bold">TexoAi</Link>
             </div>
             <div className="flex-1 pl-3">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"/>
                 </svg>
-                <a className=" normal-case  text-xl text-gray-400 font-normal">All Projects</a>
+                <Link to={'/dashboard/projects'} className=" normal-case  text-xl text-gray-400 font-normal">All Projects</Link>
             </div>
             <div className="flex-1 pr-96">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
