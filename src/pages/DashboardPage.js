6a9af3eebaf9663a55548270cdc276c33b334406
Dele Omotosho <dele@authoritywit.com>
2023-04-07 11:53:08 -0400

    fix: update navs

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,8 @@
 import {Outlet, useNavigate} from "react-router-dom";
 import Navbar from "../partials/Navbar";
 import Footer from "../partials/Footer";
+import {useEffect, useState} from "react";
+import {supabase} from "../config/supabase";
 
 const DashboardPage = () => {
 
--- src/partials/Navbar.js
+++ src/partials/Navbar.js
@@ -1,19 +1,41 @@
 import {supabase} from "../config/supabase";
 import {Link, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const Navbar = () => {
+
     const navigate = useNavigate();
+    // const [loggedInUser, setLoggedInUser] = useState(null)
+
     const handleLogout = async () => {
 
         const {error} = await supabase.auth.signOut();
-
         if (!error) navigate('/login')
+
     }
+
+    // useEffect(() => {
+    //
+    //     const getUser = async () => {
+    //         // get a logged in user
+    //         const {data: {user}} = await supabase.auth.getUser()
+    //
+    //         setLoggedInUser(user)
+    //         // console.log(user)
+    //         return user;
+    //     }
+    //
+    //     getUser()
+    //
+    // }, [])
+    //
+    // console.log(loggedInUser.user_metadata)
 
     return (
         <div className="navbar">
             <div className="flex-1 pl-3">
-                <Link to={'/dashboard/home'} className="btn btn-ghost normal-case text-2xl text-purple-800 font-bold">TexoAi</Link>
+                <Link to={'/dashboard/home'}
+                      className="btn btn-ghost normal-case text-2xl text-purple-800 font-bold">TexoAi</Link>
             </div>
             <div className="flex-1 pl-3">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -21,7 +43,8 @@ const Navbar = () => {
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"/>
                 </svg>
-                <Link to={'/dashboard/projects'} className=" normal-case  text-xl text-gray-400 font-normal">All Projects</Link>
+                <Link to={'/dashboard/projects'} className=" normal-case  text-xl text-gray-400 font-normal">All
+                    Projects</Link>
             </div>
             <div className="flex-1 pr-96">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -36,7 +59,11 @@ const Navbar = () => {
                 <div className="flex-1">
                     <a className="btn bg-orange-200 normal-case text-xl text-orange-700 font-bold">Upgrade Plan</a>
                     <a className="btn btn-ghost normal-case text-1xl text-gray-400 font-normal">20/left this week</a>
-                    <a className=" normal-case text-xl font-normal pl-3">Mahfuzul Islam Nabil</a>
+                    {
+                        // loggedInUser.user_metadata?.fullName
+                            // ? <Link to={'/dashboard/home'} className=" normal-case text-xl font-normal pl-3">{loggedInUser.user_metadata.fullName}</Link>
+                            <Link to={'/dashboard/home'} className=" normal-case text-xl font-normal pl-3">Random User</Link>
+                    }
                 </div>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
